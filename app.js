48e5227894fda0f9e3079ababe9290b390764500
Fortunato <fortunato.maycotte@gmail.com>
2017-03-08 06:44:57 -0800

    3rd

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 'use strict';
 
-var answer = prompt('Are you ready to rumble?');
+var answer = prompt('Do you like music?');
 if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-  // If the user answers yes, do the following.
-  alert('The user said they are indeed ready to rumble.');
+  alert('I also like music');
 } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
 
-  alert('The user is unfortunately not ready to rumble.');
+  alert('Mmmmmmm you are not the party type I see....');
 } else {
-  alert('You didn\'t put in a proper answer!');
+  alert('That has nothing to do with what I asked you daaaa!!!!');
+}
+var answer = prompt('Do you like animals?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('I am glad you like animals, I like them too!!!!');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+
+  alert('Wow!!!! do not talk to me anymore animal hater!!!');
+} else {
+  alert('That has nothing to do with what I asked you daaaa!!!!');
 }
