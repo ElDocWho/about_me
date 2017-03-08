f6d2b5fadb360e48d48aca6177e3543a8b53a3ca
Fortunato <fortunato.maycotte@gmail.com>
2017-03-08 07:29:57 -0800

    4

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
-'use strict';
+'use strict'
 
-var answer = prompt('Do you like music?');
-if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+var answer1 = prompt('Do you like music?');
+if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
   alert('I also like music');
-} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-
-  alert('Mmmmmmm you are not the party type I see....');
+} else if (answer1.toLowerCase() === 'no' || answer1.toLowerCase() === 'n') {
+ alert('Mmmmmmm you are not the party type I see....');
 } else {
   alert('That has nothing to do with what I asked you daaaa!!!!');
 }
-var answer = prompt('Do you like animals?');
-if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-  alert('I am glad you like animals, I like them too!!!!');
-} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
 
+console.log('Do you like music?');
+console.log(answer1);
+
+
+var answer2 = prompt('Do you like animals?');
+if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
+  alert('I am glad you like animals, I like them too!!!!');
+} else if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
   alert('Wow!!!! do not talk to me anymore animal hater!!!');
 } else {
   alert('That has nothing to do with what I asked you daaaa!!!!');
 }
+
+console.log('Do you like animals?');
+console.log(answer2);
+
+var answer3 = prompt('Do you like sports?');
+if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
+  alert('Awesssome sports are always entrataining!!!');
+} else if (answer3.toLowerCase() === 'no' || answer3.toLowerCase() === 'n') {
+  alert('Lazy!!!! Get up and do some sports');
+} else {
+  alert('That has nothing to do with what I asked you daaaa!!!!');
+}
+
+console.log('Do you like sports?');
+console.log(answer3);
+
+var answer4 = prompt('Do you like working?');
+if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
+  alert('Good, work is alwyas good for the spirit');
+} else if (answer4.toLowerCase() === 'no' || answer4.toLowerCase() === 'n') {
+  alert('You should try enjoing it.');
+} else {
+  alert('That has nothing to do with what I asked you daaaa!!!!');
+}
+
+console.log('Do you like working?');
+console.log(answer4);
+
+var answer5 = prompt('So did you like my questions?');
+if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase() === 'y') {
+  alert('I like them too, very informative');
+} else if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
+  alert('Well too bad, not gonna change them.');
+} else {
+  alert('That has nothing to do with what I asked you daaaa!!!!');
+}
+
+console.log('So did you like my questions?');
+console.log(answer5);
