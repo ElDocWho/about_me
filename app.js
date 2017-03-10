a8b7ded9bc53fbdcd4f286fdcadd3bc48bad872b
Fortunato <fortunato.maycotte@gmail.com>
2017-03-10 00:45:42 -0800

    7

--- app.js
+++ app.js
@@ -60,3 +60,66 @@ if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase() === 'y') {
 
 console.log('So did you like my questions?');
 console.log(answer5);
+
+alert('Couple more questions.......')
+
+var question6 = ('Guess a number from 0 to 9, you have 5 chances to guess it');
+var number6 = Math.floor(Math.random() * 10);
+var i;
+for (i=0; i<5; i++) {
+var answer6 = parseInt(prompt(question6));
+if (number6 === answer6) {
+alert('You won!!!!');
+var won = 1;
+break;
+}
+else {
+alert('Try again!!!');
+}
+}
+if (won === 1 ) {
+alert('Good job!!!');
+}
+else {
+alert('You lost!!! the number was ' + number6);
+}
+
+var question7 = ('What is the name of my oldest son? 3 chances to guess');
+var number7 = ['Fortunato', 'Marcela'];
+var ii;
+
+for (ii = 0; ii < 3; ii++) {
+  var answer7 = prompt(question7);
+  if (answer7 === number7[0]) {
+alert('You win!!!');
+var wonwon = 1;
+break;
+}
+else {
+alert('Wrong!!!');
+}
+}
+
+alert('last question......');
+
+var question8 = ('What is the name of my daughter? 3 chances to guess');
+var iii;
+
+for (iii = 0; iii < 3; iii++) {
+  var answer8 = prompt(question8);
+  if (answer8 === number7[1]) {
+alert('You win!!!');
+var wonwonwon = 1;
+break;
+}
+else {
+alert('Wrong!!!');
+}
+}
+
+if ((wonwon ===1) && (wonwonwon === 1)) {
+  alert('You got both names right!!!!');
+}
+else {
+  alert('You did not get both names right!!!')
+}
