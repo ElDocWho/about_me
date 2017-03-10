dc7b81935525beb58d92207f51d47004f6ac3435
Fortunato <fortunato.maycotte@gmail.com>
2017-03-10 03:19:28 -0800

    finalday4

--- app.js
+++ app.js
@@ -1,65 +1,46 @@
 "use strict";
 
-var answer1 = prompt('Do you like music?');
-if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
-  alert('I also like music');
-} else if (answer1.toLowerCase() === 'no' || answer1.toLowerCase() === 'n') {
- alert('Mmmmmmm you are not the party type I see....');
-} else {
-  alert('That has nothing to do with what I asked you daaaa!!!!');
-}
-
-console.log('Do you like music?');
+var question = ('Do you like music?');
+var pos = ('I also like music')
+var neg = ('Mmmmmmm you are not the party type I see....')
+var nnn = ('That has nothing to do with what I asked you daaaa!!!!')
+function qam() {
+  var answer1 = prompt(question);
+  if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
+    alert(pos);
+  } else if (answer1.toLowerCase() === 'no' || answer1.toLowerCase() === 'n') {
+   alert(neg);
+  } else {
+    alert(nnn);
+  }
+console.log(question);
 console.log(answer1);
-
-
-var answer2 = prompt('Do you like animals?');
-if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
-  alert('I am glad you like animals, I like them too!!!!');
-} else if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
-  alert('Wow!!!! do not talk to me anymore animal hater!!!');
-} else {
-  alert('That has nothing to do with what I asked you daaaa!!!!');
 }
+qam();
 
-console.log('Do you like animals?');
-console.log(answer2);
+var question = ('Do you like animals?');
+var pos = ('I am glad you like animals, I like them too!!!!')
+var neg = ('Wow!!!! do not talk to me anymore animal hater!!!')
+var nnn = ('That has nothing to do with what I asked you daaaa!!!!')
+qam();
 
-var answer3 = prompt('Do you like sports?');
-if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
-  alert('Awesssome sports are always entrataining!!!');
-} else if (answer3.toLowerCase() === 'no' || answer3.toLowerCase() === 'n') {
-  alert('Lazy!!!! Get up and do some sports');
-} else {
-  alert('That has nothing to do with what I asked you daaaa!!!!');
-}
+var question = ('Do you like sports?');
+var pos = ('Awesssome sports are always entrataining!!!')
+var neg = ('Lazy!!!! Get up and do some sports')
+var nnn = ('That has nothing to do with what I asked you daaaa!!!!')
+qam();
 
-console.log('Do you like sports?');
-console.log(answer3);
+var question = ('Do you like working?');
+var pos = ('Good, work is always good for the spirit')
+var neg = ('You should try enjoing it.')
+var nnn = ('That has nothing to do with what I asked you daaaa!!!!')
+qam();
 
-var answer4 = prompt('Do you like working?');
-if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
-  alert('Good, work is always good for the spirit');
-} else if (answer4.toLowerCase() === 'no' || answer4.toLowerCase() === 'n') {
-  alert('You should try enjoing it.');
-} else {
-  alert('That has nothing to do with what I asked you daaaa!!!!');
-}
-
-console.log('Do you like working?');
-console.log(answer4);
-
-var answer5 = prompt('So did you like my questions?');
-if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase() === 'y') {
-  alert('I like them too, very informative');
-} else if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
-  alert('Well too bad, not gonna change them.');
-} else {
-  alert('That has nothing to do with what I asked you daaaa!!!!');
-}
-
-console.log('So did you like my questions?');
-console.log(answer5);
+var question = ('So did you like my questions?');
+var pos = ('I like them too, very informative')
+var neg = ('Well too bad, not gonna change them.')
+var nnn = ('That has nothing to do with what I asked you daaaa!!!!')
+qam();
 
 alert('Couple more questions.......')
 
